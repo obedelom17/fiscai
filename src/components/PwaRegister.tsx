d86f4cf9baa6569ade0f4e76eb4a1a449bbf87c6
obedelom17 <agbebaviobedelom@gmail.com>
2026-08-20 11:17:21 +0000

    feat: ajout code tiers Sage (Auxiliaire) sur ligne fournisseur 40100000

--- src/components/PwaRegister.tsx
+++ src/components/PwaRegister.tsx
@@ -3,8 +3,11 @@ import { useEffect } from 'react'
 
 export default function PwaRegister() {
   useEffect(() => {
+    // Désinstaller tous les service workers existants pour éviter les problèmes de cache
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js').catch(() => {})
+      navigator.serviceWorker.getRegistrations().then(registrations => {
+        registrations.forEach(r => r.unregister())
+      })
     }
   }, [])
   return null
